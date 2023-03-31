5c9a0c4f04f8d08fefcfc8b5f98a4fdd8b462dba
Aude Jean-Baptiste <jeaaud1@alum.us.es>
2023-03-31 08:29:06 +0200

    add main component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseMapComponent } from './base-map/base-map.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BaseMapComponent
+    BaseMapComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
