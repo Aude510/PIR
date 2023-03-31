84127d719726476cf84ab32cedb340a734ea5b00
Aude Jean-Baptiste <jeaaud1@alum.us.es>
2023-03-31 18:31:43 +0200

    tracer polygone

--- front/src/app/block-zone/block-zone.component.ts
+++ front/src/app/block-zone/block-zone.component.ts
@@ -13,17 +13,22 @@ import * as L from "leaflet";
 export class BlockZoneComponent {
 
   private listePoints: Array<LatLng> = []; // ne pas oublier d'init 
-
   addPoint(event: MapMouseEvent){
+    let maxNbPts: number = 5; 
+    // let scope {}
+    // var scope par fonction (à éviter)
+    // attention const = final en java, pas constante 
     /* add marqueur sur la map de l'event à la position du point */ 
-    var marker: L.Marker = new L.Marker(event.event.latlng); 
+    let marker: L.Marker = new L.Marker(event.event.latlng); 
     event.map.addLayer(marker);
     // add point à la liste 
     this.listePoints.push(event.event.latlng); 
     // tracer polygone si plus de 3 points 
-    if (this.listePoints.length>3){
+    if (this.listePoints.length>2){
+      event.map.eachLayer((layer) => {layer.remove();}); // se démerder pour pas flush la map 
+      let polygon = L.polygon(this.listePoints).addTo(event.map);
     }
-
+    // pop up quand on dépasse le nombre de points 
   }
 
 
