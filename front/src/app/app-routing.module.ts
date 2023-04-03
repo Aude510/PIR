8d99350d365f8a6612ba6cfa7d5c288513ba918b
Aude Jean-Baptiste <jeaaud1@alum.us.es>
2023-04-03 16:50:32 +0200

    add path block-zone

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {AddDroneComponent} from "./add-drone/add-drone.component";
 
 const routes: Routes = [
   { path:'add-drone', component: AddDroneComponent },
+  { path:'block-zone', component: BlockZoneComponent},
   {path: "**",component: MainComponent}
 ];
 
