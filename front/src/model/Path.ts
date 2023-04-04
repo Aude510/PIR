8316f4a3affa4d94a0bf9eb1ed4b5fef651da03f
Joelimgu <joelimgu@gmail.com>
2023-04-04 11:17:25 +0200

    rebase path to type

--- front/src/model/Path.ts
+++ front/src/model/Path.ts
@@ -1,18 +1,6 @@
 import {Point} from "./Point";
 import {point} from "leaflet";
 
-export class Path {
+export type Path = {
     points: Point[]
-    constructor(pointList : Point[]) {
-      this.points = pointList;
-    }
-
-    addPoint(point: Point){
-      this.points.push(point);
-    }
-    clearPath(){
-      while(this.points.length > 0){
-        this.points.pop();
-      }
-    }
-  }
+}
