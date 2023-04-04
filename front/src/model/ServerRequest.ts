312b1aac1cbc93da4707cb190817d210bdb05336
Newglear <g_robert@insa-toulouse.fr>
2023-04-04 10:27:17 +0200

    send interface (new_drone, delete_drone)

--- front/src/model/ServerRequest.ts
+++ front/src/model/ServerRequest.ts
@@ -1,4 +1,4 @@
-type ServerRequestType = "answer_path" | "delete_zone" | "block_zone" |"new_drone" | "delete_drone" |"get_status"  ;
+type ServerRequestType = "answer_path" | "delete_zone" | "block_zone" |"new_drone" | "delete_drone" |"get_status" ;
 export type ServerRequest<T> = {
   type: ServerRequestType,
   data: T
