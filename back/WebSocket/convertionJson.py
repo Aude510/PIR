5eb0d81cd7d28e98132fa1669f1f28c3a788fd1b
RomainMln <moulin.romain@hotmail.com>
2023-04-04 09:27:57 +0200

    Ajout fonction de convertions + commentaires

--- back/WebSocket/convertionJson.py
+++ back/WebSocket/convertionJson.py
@@ -1,34 +1,83 @@
 import json
 
+#Error raised when we can't decode the type of the message
+class MessageTypeError(Exception):
+    pass
 
-#Prend en input un json parsé et renvoie un Drone
+#Create an error message to send to the client
+def errorMessage():
+    x={
+        "code":401
+    }
+    return json.dumps(x)
 
+#Create an ack message to send to the client
+def ackMessage():
+    x={
+        "code":200
+    }
+    return json.dumps(x)
 
-def statusToJson(owner : int, drones, blockedZones, time : int):
+#Convert from Json to type the message
+#input = str
+#return = str (type of the message) or raise MessageTypeError if error
+def jsonToType(message):
+    try:
+        y=json.loads(message)
+        return(y["type"])
+    except:
+        raise MessageTypeError()
+
+#Convert from status to Json
+#input = (owner : int, drones: drone[], blockeZones: zone[], time : int)
+#return = message : str
+def statusToJson(owner, drones, blockedZones, time):
     x={
         "owner":owner,
         "drones":drones,
         "blocked_Zones":blockedZones,
-        "time":time
+        "time":time,
+        "code":200
     }
-    return x
+    return json.dumps(x,indent=4)
 
+#Convert from JSON to Drone
+#input = message : str
+#return = ID:int, name:str, owner:int,
+#   priority:int, start:point, destination:point
+def jsonToDrone(message):
+    y=json.loads(message)
+    id=int(y["ID"])
+    name=y["name"]
+    owner=int(y["owner"])
+    priority=int(y["priority"])
+    start=y["start"]
+    destination=y["destination"]
+    return id,name,owner,priority,start,destination
+
+#Convert from Json to Owner
+#input = message : str
+#return = owner : int
 def jsonToOwner(message) -> int:
     y=json.loads(message)
     return(int(y["owner"]))
 
-########## TEST ECRITURE ######################
-fichier = open("testEcriture.json","w",encoding="utf-8")
-json.dump(statusToJson(3,5,6,7),fichier,ensure_ascii=False,indent=4)
-fichier.close()
-########## TEST LECTURE #######################
-fichier = open("testEcriture.json","r")
-contenu = fichier.read()
-print(f"Fichier json de base : {contenu}")
-test=jsonToOwner(contenu)
-print(f"Owner obtenu depuis le fichier {test}")
-fichier.close()
+#Convert from Json to Squares
+#input = message : str
+#return = square : square 
+def jsonToZone(message):
+    y=json.loads(message)
+    return y["square"]
 
-# def jsonToZone(message):
-#     y=json.loads(message)
-#     return
+########## TEST ECRITURE ######################
+# fichier = open("testEcriture.json","w",encoding="utf-8")
+# json.dump(statusToJson(3,5,6,7),fichier,ensure_ascii=False,indent=4)
+# fichier.close()
+# ########## TEST LECTURE #######################
+# fichier = open("testEcriture.json","r")
+# contenu = fichier.read()
+# print(f"Fichier json de base : {contenu}")
+# test=jsonToOwner(contenu)
+# print(f"Owner obtenu depuis le fichier {test}")
+# fichier.close()
+# print(jsonToZone("{\"squares\":[]}"))
--- back/WebSocket/server.py
+++ back/WebSocket/server.py
@@ -1,37 +1,54 @@
 import asyncio
 import websockets 
+import convertionJson
+
+connect = [] #List of all clients connected
+period = 2 #Time in seconds to send periodic message
 
 
-connect = []
-
-
-
+#Handler executed at every new connection
 async def handler(websocket,path):
-    connect.append(websocket)
-    print(path)
+    connect.append(websocket) #Add the new connection to the list of all connections  
     print("Je suis dans le handler")
     try:
-        async for messages in websocket:
-            match path[1:]:
+        async for message in websocket: #Handle every message from the connection
+            print("Message reçu !")
+            match convertionJson.jsonToType(message): #Identifies the type of the message if not, raise MessageTypeError
+                case "answer_path":
+                    pass
+                    sendUnicast("I have received answer_path",websocket)
+                    print("answer_path")
                 case "delete_zone":
-                    await sendUnicast("Tu es dans le delete_zone",websocket)
-                case "new_drone":
-                    await sendUnicast("Tu es dans le new_drone",websocket)
-                case "delete_drone":
-                    await sendUnicast("Tu es dans le delete_drone",websocket)
+                    pass
+                    sendUnicast(convertionJson.ackMessage(),websocket)
+                    print("delete_zone")
                 case "block_zone":
-                    await sendUnicast("Tu es dans le block_zone",websocket)
+                    pass
+                    sendUnicast("I have received block_zone",websocket)
+                    print("block_zone")
+                case "new_drone":
+                    id,name,owner,priority,start,destination = convertionJson.jsonToDrone(message)
+                    sendUnicast(f"I have received new_drone with ID:{id} and owner {owner}",websocket)
+                    print(f"new_drone with id:{id} and owner:{owner}")
+                case "delete_drone":
+                    pass
+                    sendUnicast("I have received delete_drone",websocket)
+                    print("delete_drone")
                 case "get_status":
-                    await sendUnicast("Tu es dans le get_status",websocket)
-                case "answer_new_path":
-                    await sendUnicast("Tu es dans le answer_new_path",websocket)
+                    pass
+                    sendUnicast("I have received get_status",websocket)
+                    print("get_status")
                 case _:
-                    await sendUnicast(f"Tu es dans le {path[1:]}, tu es perdu",websocket)
-    except websockets.exceptions.ConnectionClosed as e:  
+                    raise convertionJson.MessageTypeError
+    except websockets.exceptions.ConnectionClosed as e: #Connection closed 
         print("Session closed") 
-        connect.remove(websocket)
+        connect.remove(websocket) #Delete the connection from the list
+    except convertionJson.MessageTypeError:
+        print("Erreur sur le type du message reçu")
+        sendUnicast(convertionJson.errorMessage(),websocket) #Send an error message to the client
 
 
+#Send a message to an unique client 
 async def sendUnicast(message, websocket):
     print("Envoi Unicast")
     try:
@@ -39,6 +56,7 @@ async def sendUnicast(message, websocket):
     except websockets.exception.ConnectionClosed:
         print("Impossible to send connection was closed")
 
+#Send a message to all clients
 async def sendAllClients(message):
     print("Envoi à tous les clients")
     for websocket in connect:
@@ -47,6 +65,7 @@ async def sendAllClients(message):
         except websockets.exception.ConnectionClosed:
             print("Connection closed")
 
+#Send periodically to all clients
 async def sendPeriodically(message):
     while True:
         print("Envoi périodique")
@@ -55,16 +74,18 @@ async def sendPeriodically(message):
                 await websocket.send(message)
             except websockets.exception.ConnectionClosed:
                 print(f"Session Closed  in {__name__}")
-        await asyncio.sleep(2)
+        await asyncio.sleep(period)
         
 
+#Créer le serveur websocket et envoi périodiquement à tous les clients le statu
 async def main():
     async with websockets.serve(handler, "", 80):
-        #envoiPeriodique = asyncio.create_task(sendPeriodically("Salut !"))
-        while True:
-            #await sendAllClients("Multicast")
-            #if len(connect)>0:
-                #await sendUnicast("Unicast",connect[0])
-            await asyncio.sleep(2)
+        envoiPeriodique = asyncio.create_task(sendPeriodically("Status")) #TODO Faire en sorte qu'on envoi le statu
+        # while True:
+        #     #await sendAllClients("Multicast")
+        #     #if len(connect)>0:
+        #         #await sendUnicast("Unicast",connect[0])
+        #     await asyncio.sleep(2)
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
