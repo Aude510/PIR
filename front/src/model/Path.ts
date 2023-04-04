14d7d4fd81a836ce93190dcb10016f596911a1e9
Newglear <g_robert@insa-toulouse.fr>
2023-04-04 11:09:36 +0200

    type to class

--- front/src/model/Path.ts
+++ front/src/model/Path.ts
@@ -1,5 +1,18 @@
 import {Point} from "./Point";
+import {point} from "leaflet";
 
-export type Path = {
+export class Path {
     points: Point[]
+    constructor(pointList : Point[]) {
+      this.points = pointList;
+    }
+
+    addPoint(point: Point){
+      this.points.push(point);
+    }
+    clearPath(){
+      while(this.points.length > 0){
+        this.points.pop();
+      }
+    }
   }
