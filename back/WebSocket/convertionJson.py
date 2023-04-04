4a9e8878444b65968e878e2d31a7390bc818dc83
RomainMln <moulin.romain@hotmail.com>
2023-04-04 11:47:21 +0200

    Ajout conversion drone + debut formatage

--- back/WebSocket/convertionJson.py
+++ back/WebSocket/convertionJson.py
@@ -1,8 +1,10 @@
 import json
 
+
 #Error raised when we can't decode the type of the message
 class MessageTypeError(Exception):
     pass
+
 
 #Create an error message to send to the client
 def errorMessage():
@@ -11,10 +13,51 @@ def errorMessage():
     }
     return json.dumps(x)
 
+
 #Create an ack message to send to the client
 def ackMessage():
     x={
         "code":200
+    }
+    return json.dumps(x)
+
+
+#Convert from status to Json
+#input = (owner : int, drones: drone[], blockeZones: zone[], time : int)
+#return = message : str
+#TODO bien formater l'envoi de la liste de drone et des zones
+def statusToJson(owner, drones, blockedZones, time):
+    x={
+        "code":200,
+        "data":{
+            "owner":{"ID":owner},
+            "drones":drones,
+            "blocked_Zones":blockedZones,
+            "time":time
+        }
+    }
+    return json.dumps(x)
+
+#Convert from drone to Json
+#input = (id : int, name : str, owner : int, priority : int
+#   path : list of points([[x,y]]), start : point([x,y])
+#   destination : point([x,y]))
+#return = message : str
+def droneToJson(id, name, owner, priority, path, start, destination):
+    auxPath=[]
+    for point in path:
+        auxPath.append({"x":point[0],"y":point[1]})
+    x={
+        "code":200,
+        "data":{
+            "ID":id,
+            "name":name,
+            "owner":{"ID":owner},
+            "priority":priority,
+            "path":{"points":auxPath},
+            "start":{"x":start[0],"y":start[1]},
+            "destination":{"x":destination[0],"y":destination[1]}
+        }
     }
     return json.dumps(x)
 
@@ -24,50 +67,53 @@ def ackMessage():
 def jsonToType(message):
     try:
         y=json.loads(message)
+        print(y)
         return(y["type"])
     except:
         raise MessageTypeError()
 
-#Convert from status to Json
-#input = (owner : int, drones: drone[], blockeZones: zone[], time : int)
-#return = message : str
-def statusToJson(owner, drones, blockedZones, time):
-    x={
-        "owner":owner,
-        "drones":drones,
-        "blocked_Zones":blockedZones,
-        "time":time,
-        "code":200
-    }
-    return json.dumps(x,indent=4)
 
 #Convert from JSON to Drone
 #input = message : str
 #return = ID:int, name:str, owner:int,
-#   priority:int, start:point, destination:point
+#   priority:int, start:list[x,y], destination:list[x,y]
 def jsonToDrone(message):
     y=json.loads(message)
-    id=int(y["ID"])
-    name=y["name"]
-    owner=int(y["owner"])
-    priority=int(y["priority"])
-    start=y["start"]
-    destination=y["destination"]
+    drone=y["data"]
+    id=drone["ID"]
+    name=drone["name"]
+    owner=drone["owner"]["ID"]
+    priority=drone["priority"]
+    start=[drone["start"]["x"],drone["start"]["y"]]
+    destination=[drone["destination"]["x"],drone["destination"]["y"]]
     return id,name,owner,priority,start,destination
+
 
 #Convert from Json to Owner
 #input = message : str
 #return = owner : int
 def jsonToOwner(message) -> int:
     y=json.loads(message)
-    return(int(y["owner"]))
+    if(type(y["owner"]["ID"])==int):
+        return(y["owner"])
+    else:
+        raise MessageTypeError
+
 
 #Convert from Json to Squares
 #input = message : str
-#return = square : square 
+#return = square : list[points]
 def jsonToZone(message):
     y=json.loads(message)
-    return y["square"]
+    zone = y["data"]
+    if(len(zone["square"]["points"])==4):
+        return zone["square"]["points"]
+    else:
+        raise MessageTypeError
+
+
+#def jsonToNewPathResponse(): #TODO A implémenter (voir avec Killian et Joel)
+
 
 ########## TEST ECRITURE ######################
 # fichier = open("testEcriture.json","w",encoding="utf-8")
