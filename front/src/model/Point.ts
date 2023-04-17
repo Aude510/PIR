9ecdaea8477c5e29b7b1e8b1f941252bfe88401b
joel <joelimgu@gmail.com>
2023-04-17 22:54:39 +0200

    point functions

--- front/src/model/Point.ts
+++ front/src/model/Point.ts
@@ -1,12 +1,23 @@
 import { LatLng } from "leaflet";
 
-export class Point { 
+export class Point {
   x: number
   y: number
 
   constructor(latlng: LatLng) {
     this.x=latlng.lat;
     this.y=latlng.lng;
+  }
 
+  public static fromTuple(x: number, y: number): Point {
+    return new Point(new LatLng(x, y));
+  }
+
+  public toLatLng(): LatLng {
+    return new LatLng(this.x, this.y);
+  }
+
+  public toArray(): number[] {
+    return [this.x, this.y];
   }
 }
